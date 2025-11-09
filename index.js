428398a46bf80ce12080bcda8cfb98b7d50c4254
riks17 <rikhavsatra8@gmail.com>
2025-11-09 23:12:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello from Keyur'))
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(3000, () => console.log('App running'))
+}
+
+module.exports = app
