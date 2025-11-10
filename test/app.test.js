7a05ca4db8acb12050c76ec0ba58f6e68a19f750
riks17 <rikhavsatra8@gmail.com>
2025-11-10 13:46:50 +0530

    check pipeline

--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', () => {
-    it('should return Hello from Keyur', async () => {
+    it('should return Hello World', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
-        expect(res.text).toBe('Hello from Keyur');
+        expect(res.text).toBe('Hello World');
     });
 });
